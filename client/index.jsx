e4c67dbbfae0997ebef06d8c215732ee9b97bf45
Kevin Yang <kkaiyang94@gmail.com>
2019-03-27 20:02:10 -0700

    Configure eslint

--- client/index.jsx
+++ client/index.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render () {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  render() {
     return (
       <div>Hello World!</div>
-    )
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
