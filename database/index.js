c9d5c031e60f4c75f7d61b504bdb0f9c1fd85d17
Kevin Yang <kkaiyang94@gmail.com>
2019-03-29 15:53:53 -0700

    Set up schema for mongoose

---
+++ database/index.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/fecsampledata');
+
+const similarRestaurant = mongoose.Schema({
+  // TODO: your schema here!
+  id: { type: Number, unique: true },
+  name: String,
+  city: String,
+  cuisine: String,
+  district: String,
+  price: String,
+  zagatRating: Number,
+  googleRating: Number,
+  description: String,
+});
+
+const Restaurant = mongoose.model('restaurant', similarRestaurant);
+
+const save = (currentRestaurant, callback) => {
+  Restaurant.create(currentRestaurant, (err, success) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, success);
+    }
+  });
+};
+
+module.exports = { save };
