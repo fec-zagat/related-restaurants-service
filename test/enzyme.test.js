936b9f3e77c138d2ec1f728eb78aa42f8a56eb1b
Kevin Yang <kkaiyang94@gmail.com>
2019-03-29 16:39:36 -0700

    Fix spelling on Ezyme test

--- test/enzyme.test.js
+++ test/enzyme.test.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../client/components/App';
 
-describe('Hello Wolrd Test', () => {
+describe('Hello World Test', () => {
   const wrapper = shallow(<App />);
-  test('says hellow world', () => {
+  test('says hello world', () => {
     expect(wrapper.text()).toEqual('Hello World');
   });
 });
