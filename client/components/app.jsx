cfef119f9a774046baa0ecaf17997b548ea02314
Kevin Yang <kkaiyang94@gmail.com>
2019-04-01 15:35:33 -0700

    Implement new state property for districts

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -10,6 +10,7 @@ class App extends React.Component {
       cuisine: '',
       district: '',
       currentRestaurant: '',
+      districts: {},
     };
   }
 
@@ -19,11 +20,18 @@ class App extends React.Component {
       url: endpoint,
       method: 'GET',
       success: (query) => {
+        const newDistricts = {};
+        for (let i = 0; i < query.length; i += 1) {
+          if (newDistricts[query[i].district] === undefined) {
+            newDistricts[query[i].district] = query[i].district;
+          }
+        }
         this.setState({
           data: query,
           cuisine: query[0].cuisine,
           district: query[0].district,
           currentRestaurant: query[0].name,
+          districts: newDistricts,
         });
       },
     });
@@ -34,13 +42,14 @@ class App extends React.Component {
     const { cuisine } = this.state;
     const { district } = this.state;
     const { currentRestaurant } = this.state;
+    const { districts } = this.state;
     const type = data.filter(restaurant => restaurant.cuisine === cuisine);
     const location = type.filter(restaurant => restaurant.district === district);
     return (
-      <div>
-        <h5>RELATED PLACES</h5>
-        <div>{`More ${cuisine} near ${currentRestaurant}`}</div>
-        <RelatedRestaurant data={location} />
+      <div className="related-places">
+        <h5 className="related-places-section-title">RELATED PLACES</h5>
+        <div className="related-near-title">{`More ${cuisine} near ${currentRestaurant}`}</div>
+        <RelatedRestaurant data={location} districts={districts} />
       </div>
     );
   }
