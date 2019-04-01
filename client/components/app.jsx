0c61c65e1ebac2f103e30a9c2cc35f7efbe88337
Kevin Yang <kkaiyang94@gmail.com>
2019-04-01 16:21:10 -0700

    Implement new logic for passing down 6 restaurants

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -38,6 +38,7 @@ class App extends React.Component {
   }
 
   render() {
+    let list = [];
     const { data } = this.state;
     const { cuisine } = this.state;
     const { district } = this.state;
@@ -45,11 +46,29 @@ class App extends React.Component {
     const { districts } = this.state;
     const type = data.filter(restaurant => restaurant.cuisine === cuisine);
     const location = type.filter(restaurant => restaurant.district === district);
+    if (location.length === 6) {
+      list = location;
+    } else if (location.length > 6) {
+      list = location.slice(0, 6);
+    } else if (location.length < 6) {
+      const additionalRestaurants = data.filter(restaurant => restaurant.district === district);
+      for (let i = 0; i < additionalRestaurants.length; i += 1) {
+        const restaurant = additionalRestaurants[i];
+        if (!location.includes(restaurant)) {
+          location.push(restaurant);
+        }
+        if (location.length > 5) {
+          break;
+        }
+      }
+      list = location;
+    }
+
     return (
       <div className="related-places">
         <h5 className="related-places-section-title">RELATED PLACES</h5>
         <div className="related-near-title">{`More ${cuisine} near ${currentRestaurant}`}</div>
-        <RelatedRestaurant data={location} districts={districts} />
+        <RelatedRestaurant data={list} districts={districts} />
       </div>
     );
   }
