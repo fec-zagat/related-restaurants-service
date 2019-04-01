9382e81b10fa4eada7e71eab8324c4189ba57678
Kevin Yang <kkaiyang94@gmail.com>
2019-04-01 11:39:37 -0700

    Implement Filter for cuisine and district

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,15 +1,47 @@
 import React from 'react';
+import $ from 'jquery';
+import RelatedRestaurant from './RelatedRestaurant';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      data: [],
+      cuisine: '',
+      district: '',
+      currentRestaurant: '',
     };
   }
 
+  componentDidMount() {
+    const endpoint = '/api/restaurants/';
+    $.ajax({
+      url: endpoint,
+      method: 'GET',
+      success: (query) => {
+        this.setState({
+          data: query,
+          cuisine: query[0].cuisine,
+          district: query[0].district,
+          currentRestaurant: query[0].name,
+        });
+      },
+    });
+  }
+
   render() {
+    const { data } = this.state;
+    const { cuisine } = this.state;
+    const { district } = this.state;
+    const { currentRestaurant } = this.state;
+    const type = data.filter(restaurant => restaurant.cuisine === cuisine);
+    const location = type.filter(restaurant => restaurant.district === district);
     return (
-      <div>Hello World</div>
+      <div>
+        <h5>RELATED PLACES</h5>
+        <div>{`More ${cuisine} near ${currentRestaurant}`}</div>
+        <RelatedRestaurant data={location} />
+      </div>
     );
   }
 }
