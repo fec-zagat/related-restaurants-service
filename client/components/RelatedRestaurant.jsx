4d68f9cf56bcccbd3564238f445974d37087557c
Kevin Yang <kkaiyang94@gmail.com>
2019-04-01 16:35:49 -0700

    Remove List logic from RelatedRestaurants to App

--- client/components/RelatedRestaurant.jsx
+++ client/components/RelatedRestaurant.jsx
@@ -11,23 +11,10 @@ class RelatedRestaurant extends React.Component {
 
   render() {
     const { data } = this.props;
-    const displaySix = () => {
-      let displayRestaurants = [];
-      if (data.length > 6) {
-        for (let i = 0; i < 6; i += 1) {
-          displayRestaurants.push(data[i]);
-        }
-      } else if (data.length < 6) {
-        // need to implement logic for this case
-        displayRestaurants = data;
-      } else {
-        displayRestaurants = data;
-      }
-      return displayRestaurants;
-    };
+
     return (
       <div className="related-restaurants-section">
-        {displaySix().map(review => (
+        {data.map(review => (
           <div key={review._id} className="single-restaurant">
             <h5>{review.name}</h5>
             <div>{`${review.cuisine} ${review.district}`}</div>
