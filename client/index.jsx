dc77d21a3dd0082498d45b6fa11a2869aaa7f9ce
Kevin Yang <kkaiyang94@gmail.com>
2019-04-09 21:00:52 -0700

    Add comments for proxy

--- client/index.jsx
+++ client/index.jsx
@@ -3,3 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+/*
+Uncomment for proxy
+import App from './components/App';
+window.RelatedRestaurantApp = App;
+*/
