ac5f6f53f2ac4f9d7004e24b52727205661c49c4
Kevin Yang <kkaiyang94@gmail.com>
2019-04-13 13:57:02 -0700

    Change file for docker setup

--- client/index.jsx
+++ client/index.jsx
@@ -1,11 +1,11 @@
 // Uncomment for non-proxy use
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './components/App';
-
-// ReactDOM.render(<App />, document.getElementById('app'));
-
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './components/App';
 
-window.RelatedRestaurantApp = App;
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
+// import App from './components/App';
+
+// window.RelatedRestaurantApp = App;
