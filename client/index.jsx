c06d1273ab4a089b7b8d5f876e648d89420f4265
Kevin Yang <kkaiyang94@gmail.com>
2019-07-10 15:50:34 -0700

    Modify files for local use

--- client/index.jsx
+++ client/index.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './components/App';
 
-window.RelatedRestaurantApp = App;
+ReactDOM.render(<App />, document.getElementById('app'));
