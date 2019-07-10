886382a21976cb59fdc9e465b19b870bbeabfc16
Kevin Yang <kkaiyang94@gmail.com>
2019-07-10 16:12:25 -0700

    Implement controllers

--- server/index.js
+++ server/index.js
@@ -4,9 +4,11 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3005;
+const PORT = 3004;
 
-const db = require('../database/index');
+const {
+  seedData, filterDistrictAndCuisine, filterCuisine, findById,
+} = require('./controllers/databaseControllers');
 
 app.use(cors());
 app.use(bodyParser.urlencoded());
@@ -14,62 +16,13 @@ app.use(bodyParser.json());
 app.use('/r/:restaurant', express.static(path.join(__dirname, '../public')));
 
 
-app.post('/api/restaurants/', (req, res) => {
-  const restaurantInfo = {
-    id: req.body.id,
-    name: req.body.name,
-    city: req.body.city,
-    cuisine: req.body.cuisine,
-    district: req.body.district,
-    price: req.body.price,
-    zagatRating: req.body.zagatRating,
-    googleRating: req.body.googleRating,
-    description: req.body.description,
-  };
-  db.save(restaurantInfo, (err, success) => {
-    if (err) {
-      return;
-    }
-    res.send('query was an success', success);
-  });
-});
+app.post('/api/restaurants/', seedData);
 
-app.get('/api/restaurants/:district/:cuisine', (req, res) => {
-  const options = {
-    cuisine: req.params.cuisine,
-    district: req.params.district,
-  };
-  db.retrieve(options, (err, success) => {
-    if (err) {
-      res.status(400).send(err);
-    }
-    res.status(200).send(success);
-  });
-});
+app.get('/api/restaurants/:district/:cuisine', filterDistrictAndCuisine);
 
-app.get('/api/restaurants/:cuisine', (req, res) => {
-  const options = {
-    cuisine: req.params.cuisine,
-  };
-  db.retrieve(options, (err, success) => {
-    if (err) {
-      res.status(400).send(err);
-    }
-    res.status(200).send(success);
-  });
-});
+app.get('/api/restaurants/:cuisine', filterCuisine);
 
-app.get('/api/:id/', (req, res) => {
-  const options = {
-    id: req.params.id,
-  };
-  db.searchByID(options, (err, success) => {
-    if (err) {
-      res.status(400).send(err);
-    }
-    res.status(200).send(success);
-  });
-});
+app.get('/api/:id/', findById);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
