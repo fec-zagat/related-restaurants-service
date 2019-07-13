856da118d584d80ae5a3dced26408c816295d1b0
Kevin Yang <kkaiyang94@gmail.com>
2019-07-13 14:37:33 -0700

    Modify Ratings component to stateless

--- client/components/Ratings.jsx
+++ client/components/Ratings.jsx
@@ -2,33 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../../styles/ratings.css';
 
-class Ratings extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
-  render() {
-    const { zagatRating, googleRating } = this.props;
-
-    return (
-      <div className="ratings-line">
-        <div className="tagaz-logo" alt="" />
-        <div className="zagat-rating-text">{zagatRating > 0 ? ' FOOD ' : 'ZAGAT RATED'}</div>
-        <div className="zagat-rating-number">{zagatRating}</div>
-        <div className="ratings-divider" />
-        <div className="google-logo" />
-        <div className="google-rating-text">{googleRating}</div>
-        <div className="stars">
-          <div className="empty-stars" />
-          <div className="full-stars" style={{ width: `${googleRating / 5 * 100}%` }} />
-        </div>
-      </div>
-    );
-  }
-}
+const Ratings = ({ zagatRating, googleRating }) => (
+  <div className="ratings-line">
+    <div className="tagaz-logo" alt="" />
+    <div className="zagat-rating-text">{zagatRating > 0 ? ' FOOD ' : 'ZAGAT RATED'}</div>
+    <div className="zagat-rating-number">{zagatRating}</div>
+    <div className="ratings-divider" />
+    <div className="google-logo" />
+    <div className="google-rating-text">{googleRating}</div>
+    <div className="stars">
+      <div className="empty-stars" />
+      <div className="full-stars" style={{ width: `${googleRating / 5 * 100}%` }} />
+    </div>
+  </div>
+);
 
 Ratings.propTypes = {
   zagatRating: PropTypes.number.isRequired,
